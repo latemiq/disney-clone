209366812a51fdbd3104832fd4203914e0382545
latemiq <dzierzkowskij@gmail.com>
2024-05-10 14:11:39 +0200

    Header done visualy

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,91 +1,110 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
-    function Header() {
-        return (
-            <Nav>
-                <Logo src="/images/logo.svg"/>
-                <NavMenu>
-                    <ul>
-                        <li>
-                            <a>
-                                <img src='/images/home-icon.svg'/>
-                                <p className='page-nav.page-bav--small.text-color--primary.margin--left-2'>STRONA GŁÓWNA</p>
-                            </a>
-                        </li>
-                        <li>
-                            <a>
-                                <img src='/images/search-icon.svg'/>
-                                <p>WYSZUKAJ</p>
-                            </a>
-                        </li>
-                        <li>
-                            <a>
-                                <img src='/images/watchlist-icon.svg'/>
-                                <p>MOJA LISTA</p>
-                            </a>
-                        </li>
-                        <li>
-                            <a>
-                                <img src='/images/movie-icon.svg'/>
-                                <p>FILMY</p>
-                            </a>
-                        </li>
-                        <li>
-                            <a>
-                                <img src='/images/series-icon.svg'/>
-                                <p>SERIALE</p>
-                            </a>
-                        </li>
-                        <li>
-                            <a>
-                                <img src='/images/original-icon.svg'/>
-                                <p>ORIGINALS</p>
-                            </a>
-                        </li>
-                    </ul>
-                </NavMenu>
-            </Nav>
-        )
-    }
+const Nav = styled.div`
+  width: 100%;
+  height: 70px;
+  background-image: url("/public/images/home-background.png");
+  background-position: center;
+  color: white;
+  font-size: 34pt;
+  display: flex;
 
-    export default Header
-
-    const Nav = styled.div`
-    height: 70px;
-    background: #090b13;
+`;
+const NavMenu = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: stretch;  
+  span{
     display: flex;
     align-items: center;
-    padding: 0 36px;
-    `
-    const Logo = styled.img`
-    width: 80px;
-    `
-    const NavMenu = styled.div`
-        padding: 0 36px;
-        font-size: 13px
-        letter-spacing: 1.42px;
-        line-height: 1.07;
-        padding:16px 18px;
-        p{
-            color: F9F9F9;
-            font: 13px Inspire-Bold sans-serif;
-            font-family: bold;
-            margin: 13px 0px 13px 8px;
-            padding: 2px 0px;
-            margin-left: 0.5rem;
-        }
-        a{
-            display: flex;
-        }
-        img{
-            width:24px;
-            height:20px;
-        }
-        li{
-            display: inline-block;
-            list-style: none;
-            padding: 16px 18px;
-            margin: 36px;
-        }
-    `
+  }
+
+  a{
+    display: flex;
+    justify-content: center;
+    margin: 25px;
+  }
+
+  a img{
+    width: 24px;
+  }
+  span p{
+    display: flex;
+    font-size: 13px;
+    letter-spacing: 1.42px;
+    font-family: verdana;
+    font-weight: bold;
+  }
+`
+const Logo = styled.img`
+  width: 86px;
+  height: 48px;
+  margin: 0 36px;
+  margin-top: 15px;
+`
+const RightMenu = styled.div`
+display: flex;
+align-items: center;
+margin-left: 610px;
+font-family: century;
+font-size: 15px;
+.image{
+  margin-left: 5px;
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+}
+`
+
+function Header() {
+  return (
+  <Nav>
+    <NavMenu>
+    <Logo src="/images/logo.svg" />
+     <span>
+        <a>
+          <img src="/images/home-icon.svg" />
+          <p>STRONA GŁÓWNA</p>
+        </a>
+     </span>
+     <span>
+        <a>
+          <img src="/images/search-icon.svg" />
+          <p>WYSZUKAJ</p>
+        </a>
+     </span>
+     <span>
+        <a>
+          <img src="/images/watchlist-icon.svg" />
+          <p>MOJA LISTA</p>
+        </a>
+     </span>
+     <span>
+        <a>
+          <img src="/images/movie-icon.svg" />
+          <p>FILMY</p>
+        </a>
+     </span>
+     <span>
+        <a>
+          <img src="/images/series-icon.svg" />
+          <p>SERIALE</p>
+        </a>
+     </span>
+     <span>
+        <a>
+          <img src="/images/original-icon.svg" />
+          <p>ORIGINALS</p>
+        </a>
+      </span>
+      <RightMenu>
+        <span>Johny</span>
+        <img className='image' src='/images/disney.png'/>
+      </RightMenu>
+    </NavMenu>
+  </Nav>
+  );
+}
+
+export default Header;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const Diy = styled.div`
+    border: 1px solid red;
+    display: flex;
+    color: white;
+    font-size: 46pt;
+`
+
     function Home() {
         return (
-            <div>
+            <Diy>
                 Home
-            </div>
+            </Diy>
         )
     }
 
