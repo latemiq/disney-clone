fc9c73420d062f15f5d7524416d088b47d45db49
latemiq <dzierzkowskij@gmail.com>
2024-05-13 17:09:49 +0200

    Slider done

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,106 +4,111 @@ import styled from 'styled-components';
 const Nav = styled.div`
   width: 100%;
   height: 70px;
-  background-image: url("/public/images/home-background.png");
-  background-position: center;
   color: white;
   font-size: 34pt;
   display: flex;
-
+  overflow-x: hidden;
 `;
+
 const NavMenu = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: row;
-  align-items: stretch;  
-  span{
-    display: flex;
-    align-items: center;
-  }
+  align-items: stretch;
 
-  a{
+  a {
     display: flex;
     justify-content: center;
-    margin: 25px;
+    margin: 20px;
+    cursor: pointer;
   }
 
-  a img{
+  a img {
     width: 24px;
   }
-  span p{
+
+  a p {
+    position: relative;
     display: flex;
     font-size: 13px;
-    letter-spacing: 1.42px;
+    letter-spacing: 1.32px;
     font-family: verdana;
-    font-weight: bold;
+    font-weight: 600;
+    margin-left: 5px;
   }
-`
+
+  ul {
+    position: relative;
+    right: -29.3%;
+    justify-content: flex-start;
+    display: flex;
+    align-items: center;
+    font-family: verdana;
+    font-size: 13px;
+  }
+
+  .image {
+    margin-left: 16%;
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+  }
+`;
+
 const Logo = styled.img`
   width: 86px;
   height: 48px;
   margin: 0 36px;
   margin-top: 15px;
-`
-const RightMenu = styled.div`
-display: flex;
-align-items: center;
-margin-left: 610px;
-font-family: century;
-font-size: 15px;
-.image{
-  margin-left: 5px;
-  width: 48px;
-  height: 48px;
-  border-radius: 50%;
-}
-`
+`;
 
 function Header() {
   return (
-  <Nav>
-    <NavMenu>
-    <Logo src="/images/logo.svg" />
-     <span>
-        <a>
-          <img src="/images/home-icon.svg" />
-          <p>STRONA GŁÓWNA</p>
-        </a>
-     </span>
-     <span>
-        <a>
-          <img src="/images/search-icon.svg" />
-          <p>WYSZUKAJ</p>
-        </a>
-     </span>
-     <span>
-        <a>
-          <img src="/images/watchlist-icon.svg" />
-          <p>MOJA LISTA</p>
-        </a>
-     </span>
-     <span>
-        <a>
-          <img src="/images/movie-icon.svg" />
-          <p>FILMY</p>
-        </a>
-     </span>
-     <span>
-        <a>
-          <img src="/images/series-icon.svg" />
-          <p>SERIALE</p>
-        </a>
-     </span>
-     <span>
-        <a>
-          <img src="/images/original-icon.svg" />
-          <p>ORIGINALS</p>
-        </a>
-      </span>
-      <RightMenu>
-        <span>Johny</span>
-        <img className='image' src='/images/disney.png'/>
-      </RightMenu>
-    </NavMenu>
-  </Nav>
+    <Nav>
+      <NavMenu className='row'>
+        <Logo className="col-sm" src="/images/logo.svg" />
+        <div className="col-sm bg-primary text-white">
+          <a>
+            <img src="/images/home-icon.svg" />
+            <p>STRONA GŁÓWNA</p>
+          </a>
+        </div>
+        <div className="col-sm">
+          <a>
+            <img src="/images/search-icon.svg" />
+            <p>WYSZUKAJ</p>
+          </a>
+        </div>
+        <div className="col-sm">
+          <a>
+            <img src="/images/watchlist-icon.svg" />
+            <p>MOJA LISTA</p>
+          </a>
+        </div>
+        <div className="col-sm">
+          <a>
+            <img src="/images/movie-icon.svg" />
+            <p>FILMY</p>
+          </a>
+        </div>
+        <div className="col-sm">
+          <a>
+            <img src="/images/series-icon.svg" />
+            <p>SERIALE</p>
+          </a>
+        </div>
+        <div className="col-sm">
+          <a>
+            <img src="/images/original-icon.svg" />
+            <p>ORIGINALS</p>
+          </a>
+        </div>
+        <ul>
+          <span>dzieciaki</span>
+          <img className='image' src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/F73277B06EE997988A5DD3356F145DB4A0D9C82A7656796DB20B93B2D0DC7C79/scale?width=96&format=png'/>
+        </ul>
+      </NavMenu>
+    </Nav>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,34 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import ImgSlider from './ImgSlider.js';
 
-const Diy = styled.div`
-    border: 1px solid red;
-    display: flex;
-    color: white;
-    font-size: 46pt;
-`
+
 
     function Home() {
         return (
-            <Diy>
-                Home
-            </Diy>
+            <Container>
+                <ImgSlider />
+            </Container>
         )
     }
 
-    export default Home
+    export default Home;
+
+    const Container = styled.main`
+    min-height: calc(100vh - 70px);
+    padding: 0 calc(3.5vh + 5px);
+    position: relative;
+    overflow-x: hidden;
+    &:before {
+        background: url("/images/home-background.png") center center / cover
+         no-repeat fixed;
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0; 
+        bottom: 0;
+        z-index: -1;
+    }
+
+`    
